0c774b9e7c51f7b24813fc8e80739a47fc7c36a9
pablojosecodes <pablojrhansen@gmail.com>
2023-03-01 20:28:51 -0800

    slightly cleaner

--- administerSAT.py
+++ administerSAT.py
@@ -8,7 +8,7 @@ import os
 
 import pandas as pd
 
-COMPLETIONS_MODEL = "text-davinci-003"
+COMPLETIONS_MODEL = "text-davinci-002"
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 
@@ -28,21 +28,22 @@ COMPLETIONS_API_PARAMS = {
     "model": COMPLETIONS_MODEL,
 }
 
-def answer_reading(
-    persona: str,
-    show_prompt: bool = False
-) -> str:
-    
+
+def test(answer, realanswer):
+    right = 0
+    wrong = 0
+    for i in range(len(answer)):
+        if answer[i] == realanswer[i]:
+            right = right + 1
+        else:
+            wrong = wrong + 1
+    return (right, wrong)
+
+def get_answers(dct, persona):
     initialPrompt = "I am going to give you an examination. Answer the following questions from the perspective of " +  persona + " with ONLY the correct letters.- do not articulate your reasoning behind the answers. I repeat, only respond with the letters \"A\", \"B\", \"C\", or \"D\""
-
     overarchingAnswer = ""
-
-    reading_file = open("reading.json", "r")
-    reading_questions = json.load(reading_file)   
-
-
-    for i in range(7):
-        query = initialPrompt + "\n" + "\n".join(reading_questions[str(i)])
+    for i in range(len(dct)):
+        query = initialPrompt + "\n" + "\n".join(dct[str(i)])
 
         response = openai.Completion.create(
                     prompt=query,
@@ -62,53 +63,36 @@ def answer_reading(
             if (len(toAdd) > 0):
                 toAdd = toAdd[0]
             overarchingAnswer = overarchingAnswer + toAdd
-   
+            
     answer = ' '.join(filter(whitelist.__contains__, overarchingAnswer))[:87]
     answer = answer.split(" ")
-    
-    right, wrong = test(answer, readinganswer)
-    print("right: " + str(right))
-    print("wrong: " + str(wrong))
-
-def test(answer, realanswer):
-    right = 0
-    wrong = 0
-    for i in range(len(answer)):
-        if answer[i] == realanswer[i]:
-            right = right + 1
-        else:
-            wrong = wrong + 1
-    return (right, wrong)
-
+    return answer
 
 def answer_writing(
     persona: str,
     show_prompt: bool = False
 ) -> str:
-    initialPrompt = "I am going to give you an examination. Answer the following questions from the perspective of " +  persona + " with ONLY the correct letters.- do not articulate your reasoning behind the answers. I repeat, only respond with the letters \"A\", \"B\", \"C\", or \"D\""
-    
-    overarchingAnswer = ""
+    writing_file = open("writing.json", "r")
+    writing_questions = json.load(writing_file)
 
-    f = open("writing.json", "r")
-    d = json.load(f)
-    
-    for i in range(5):
-        query = initialPrompt + "\n" + "\n".join(d[str(i)])
-
-        response = openai.Completion.create(
-                    prompt=query,
-                    **COMPLETIONS_API_PARAMS
-            )   
-        
-        overarchingAnswer = overarchingAnswer + response["choices"][0]["text"]
-    
-    
-    answer = ' '.join(filter(whitelist.__contains__, overarchingAnswer))[:103]
-    answer = answer.split(" ")
+    answer = get_answers(writing_questions, persona)
     right, wrong = test(answer, writinganswer)
-    
     print("right: " + str(right))
     print("wrong: " + str(wrong))
+
+
+def answer_reading(
+    persona: str,
+    show_prompt: bool = False
+) -> str:
+    reading_file = open("reading.json", "r")
+    reading_questions = json.load(reading_file)   
+
+    answer = get_answers(reading_questions, persona)
+    right, wrong = test(answer, writinganswer)
+    print("right: " + str(right))
+    print("wrong: " + str(wrong))
+
 
 def main():
     answer_writing(sys.argv[1])
